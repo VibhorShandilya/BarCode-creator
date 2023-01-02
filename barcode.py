8b3cf959a22f5fa3fc8f0f8fc8178430c4b4f36c
Vibhor shandilya <Vibhor155hitit2020@gmail.com>
2023-01-02 21:25:32 +0530

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -4,6 +4,6 @@
 import pyqrcode
 import png
 from pyqrcode import QRCode
-vini = "https://forms.gle/2hYVeCLxsiw4hMuf7"
+vini = "https://www.linkedin.com/in/vibhor-shandilya-032801164/"
 url=pyqrcode.create(vini)
 url.png("taru2.png",scale=6)
