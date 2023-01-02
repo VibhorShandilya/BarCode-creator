7bfa161c8fb84bafca414a5e78e39da64a4a43bf
Vibhor shandilya <Vibhor155hitit2020@gmail.com>
2023-01-02 21:29:54 +0530

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -1,9 +1,9 @@
-//module to be install
-//1)pyqrcode :- pip install pyqrcode 2)png :- pip install pypng
-
+#You should have python installed in your desktop/laptop
+# module to be install
+# 1)pyqrcode :- pip install pyqrcode 2)png :- pip install pypng
 import pyqrcode
 import png
 from pyqrcode import QRCode
 vini = "https://www.linkedin.com/in/vibhor-shandilya-032801164/"
 url=pyqrcode.create(vini)
-url.png("taru2.png",scale=6)
+url.png("anyname.png",scale=6)
